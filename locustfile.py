f192044912555b8fca8b3a129d52a19b4b28c400
Aga <aga.kapluk97@gmail.com>
2025-08-14 22:22:54 +0200

    Add classes in locustfile.py to run tests

--- locustfile.py
+++ locustfile.py
@@ -1,1 +1,9 @@
-from locust import HttpUser, task, between
+from locust import HttpUser, between, task
+from users.shopper_user import AnonymousShopperUser, LoggedInShopperUser
+
+class AnonymousShopperLocust(AnonymousShopperUser):
+    wait_time = between(1, 3)
+
+class LoggedInShopperLocust(LoggedInShopperUser):
+    wait_time = between(1, 3)
+
